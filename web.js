44c7ed55d39f1d08d7a3e1a45a691f9761307842
Wilhelm Kleu <Wilhelm.Kleu@Loan-6560b.tasima.co.za>
2012-08-22 21:41:58 +0200

    basic chat

--- web.js
+++ web.js
@@ -2,11 +2,32 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var io = require('socket.io').listen(app);
+
+app.get('/',
+ function(request, response) {
+  response.sendfile(__dirname + '/index.html');
 });
+
+app.get('/css/bootstrap.min.css',
+ function(request, response) {
+  response.sendfile(__dirname + '/bootstrap/css/bootstrap.min.css');
+});
+
+app.get('/js/bootstrap.min.js',
+ function(request, response) {
+  response.sendfile(__dirname + '/bootstrap/js/bootstrap.min.js');
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('chat', function (msg) {
+    console.log('chat received: ' + msg);
+	socket.emit('chat', msg);
+  });
 });
