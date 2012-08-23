e63df00efbd1812c53e545ddc8f0ef3b31206014
Wilhelm Kleu <wkleu@github>
2012-08-23 15:52:59 +0200

    use CDN for css/js. implement jquery. send message on enter

--- web.js
+++ web.js
@@ -9,16 +9,6 @@ app.get('/',
   response.sendfile(__dirname + '/index.html');
 });
 
-app.get('/css/bootstrap.min.css',
- function(request, response) {
-  response.sendfile(__dirname + '/bootstrap/css/bootstrap.min.css');
-});
-
-app.get('/js/bootstrap.min.js',
- function(request, response) {
-  response.sendfile(__dirname + '/bootstrap/js/bootstrap.min.js');
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
